aa175c6a07f8c44aae351099861e6ae91fc91670
pksoftware <info@pksoftware.de>
2014-04-02 21:42:31 +0200

    docs

--- app/controllers/Home.controller.js
+++ app/controllers/Home.controller.js
@@ -5,10 +5,17 @@ sap.ui.controller("de_pksoftware.ui5strap_docs.controllers.Home", {
 	},
 
 	switchTheme : function(oEvent){
-		var newTheme = oEvent.getSource().getCustomData()[0].getValue('theme');
+		var btn = oEvent.getSource();
+		var newTheme = btn.getCustomData()[0].getValue('theme');
 		if('default' === newTheme){
 			newTheme = de_pksoftware.ui5strap_docs.StrapApp.getInstance().sheets[0];
 		}
+		if(!this.activeButton){
+			this.activeButton = this.getView().byId('defaultThemeButton');
+		}
+		this.activeButton.setActive(false);
+		btn.setActive(true);
+		this.activeButton = btn;
 		jQuery.sap.includeStyleSheet(newTheme, 'style-sheet-0', null, null);
 		
 	}
