7c05fb8ef642f335a70439e07d72aac69a6d7a75
pksoftware <info@pksoftware.de>
2014-05-08 19:12:18 +0200

    themes BP

---
+++ app/controllers/Themes.controller.js
@@ -0,0 +1,59 @@
+sap.ui.controller("com_mycompany.my_app.controllers.Themes", {
+
+	app : liberty.getViewer().getApp(),
+
+	switchTheme : function(oEvent){
+		var app = this.app;
+		app.setLoaderVisible(true);
+		
+		var btn = oEvent.getSource();
+		var newTheme = btn.getCustomData()[0].getValue('theme');
+		if('default' === newTheme){
+			newTheme = jQuery.sap.getModulePath('ui5strap') + '/bootstrap-3.1.1-dist/css/bootstrap.min.css';
+		}
+		if(!this.activeButton){
+			this.activeButton = this.getView().byId('defaultThemeButton');
+		}
+		this.activeButton.setSelected(false);
+		btn.setSelected(true);
+		this.activeButton = btn;
+		jQuery.sap.includeStyleSheet(newTheme, 'ui5strap-css-0', function(){
+			app.setLoaderVisible(false);
+		}, null);
+	},
+
+	submitThemeForm : function(oEvent){
+		var app = this.app;
+		
+
+		var newTheme = this.getView().byId('themeInput').getValue();
+
+		if('' === newTheme){
+			return false;
+		}
+
+		if(!this.isValidBootstrapTheme(newTheme)){
+			//Todo Change to BS Modal
+
+			alert(newTheme + ' is not a valid Bootstrap theme!');
+
+			return false;
+		}
+		else{
+			app.setLoaderVisible(true);
+		
+			if(this.activeButton){
+				this.activeButton.setSelected(false);
+			}
+
+			jQuery.sap.includeStyleSheet(newTheme, 'ui5strap-css-0', function(){
+				app.setLoaderVisible(false);
+			}, null);
+		}
+	},
+
+	isValidBootstrapTheme : function(themeUrl){
+ 		return /bootstrap(\.min)?\.css$/.test(themeUrl);
+	}
+	
+});
