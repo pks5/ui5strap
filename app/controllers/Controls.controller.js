238507fb384c20e66690f04b2b6076add4bccf53
pksoftware <info@pksoftware.de>
2014-05-25 02:50:30 +0200

    fixes

--- app/controllers/Controls.controller.js
+++ app/controllers/Controls.controller.js
@@ -66,6 +66,72 @@ sap.ui.controller("com_mycompany.my_app.controllers.Controls", {
 
 	tooltipHidden : function(oEvent){
 		this.getView().byId('tooltipEventIndicator').setText('Tooltip hidden!');
+	},
+
+	startProgressBar : function(id, callback){
+		var _this = this;
+		var bar = this.getView().byId(id).getFirstBar();
+		var startValue = bar.getValue();
+		var value = startValue;
+		var endValue = bar.getMaxValue();
+		
+		var interval = window.setInterval(function(){
+			value = value + 2;
+			if(value >= endValue){
+				value = endValue;
+				bar.setValue(value);
+				window.clearInterval(interval);
+				window.setTimeout(callback, 1000);
+			}
+			else{
+				bar.setValue(value);
+			}
+		}, 100);
+	},
+
+	resetBars : function(button){
+		button.setSelected(false);
+		button.setSeverity(ui5strap.Severity.Success);
+			button.setText('Start Progress');
+			var icon = button.getContent()[0];
+			icon.setIcon('play');
+			icon.setSpin(false);
+			button.setEnabled(true);
+			this.getView().byId('progress1').getFirstBar().setValue(30);
+			this.getView().byId('progress2').getFirstBar().setValue(20);
+			this.getView().byId('progress3').getFirstBar().setValue(40);
+			this.getView().byId('progress4').getFirstBar().setValue(60);
+			this.getView().byId('progress5').getFirstBar().setValue(80);
+	},
+
+	startProgress : function(oEvent){
+		var _this = this;
+		var button = oEvent.getSource();
+		if(!button.getSelected()){
+			button.setSelected(true);
+			var icon = button.getContent()[0];
+			icon.setIcon('spinner');
+			icon.setSpin(true);
+			button.setSeverity(ui5strap.Severity.Danger);
+			button.setText('Please wait...');
+			button.setEnabled(false);
+			var callI = 5;
+			var callback = function(){
+				callI --;
+				if(callI === 0){
+					_this.resetBars(_this.getView().byId('progressButton'));
+				}
+			};
+			this.startProgressBar('progress1',callback);
+			this.startProgressBar('progress2',callback);
+			this.startProgressBar('progress3',callback);
+			this.startProgressBar('progress4',callback);
+			this.startProgressBar('progress5',callback);
+		}
+		else{
+			this.resetBars(button);
+		}
+
 	}
 	
 });
