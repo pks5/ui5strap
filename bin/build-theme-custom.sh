09d041be34957f419cdc918b14d14db318835f8a
Jan Philipp Knoeller <info@pksoftware.de>
2015-11-07 19:54:09 +0100

    theme building

--- bin/build-theme-custom.sh
+++ bin/build-theme-custom.sh
@@ -4,6 +4,20 @@ cd "$(dirname "$0")"
 
 cd "../tools/theme-builder/bootstrap-3.3.5"
 
-npm run build-theme-custom -- --ui5strap-theme=$1
+theme=$1
+
+if [ -z "$theme" ]
+  then
+    read -p "Theme Name: " theme
+fi
+
+if [ -z "$theme" ]
+  then
+  	  theme=ui5strap_default
+fi
+
+echo "Building Theme: $theme"
+
+npm run build-theme-custom -- --ui5strap-theme=$theme
 
 cd $PWD
