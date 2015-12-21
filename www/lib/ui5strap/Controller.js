b886e655ee19f5bf2ef773d465247a9babb3a75e
Jan Philipp Knoeller <info@pksoftware.de>
2015-12-21 17:26:42 +0100

    controller

---
+++ www/lib/ui5strap/Controller.js
@@ -0,0 +1,37 @@
+/*
+ * 
+ * UI5Strap
+ *
+ * ui5strap.Controller
+ * 
+ * @author Jan Philipp Knöller <info@pksoftware.de>
+ * 
+ * Homepage: http://ui5strap.com
+ *
+ * Copyright (c) 2013-2014 Jan Philipp Knöller <info@pksoftware.de>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Released under Apache2 license: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * 
+ */
+
+sap.ui.define(['./library', './AppBase', 'sap/ui/core/mvc/Controller'], function(library, AppBase, Controller){
+
+	var controllerImpl = {};
+	
+	AppBase.blessController(controllerImpl);
+
+	//Return Module Constructor
+	return Controller.extend("ui5strap.Controller", controllerImpl);
+
+});
