257a1be21dc5a0e38135aabab84968199c181e42
Jan Philipp Knoeller <info@pksoftware.de>
2016-01-04 22:06:46 +0100

    ListItemBase

---
+++ www/lib/ui5strap/ListItemBase.js
@@ -0,0 +1,59 @@
+/*
+ * 
+ * UI5Strap
+ *
+ * ui5strap.ListItemBase
+ * 
+ * @author Jan Philipp Knöller <info@pksoftware.de>
+ * 
+ * Homepage: http://ui5strap.com
+ *
+ * Copyright (c) 2013-2014 Jan Philipp Knöller <info@pksoftware.de>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Released under Apache2 license: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * 
+ */
+
+sap.ui.define(['./library', './ControlBase', './SelectableSupport'], function(library, ControlBase, SelectableSupport){
+
+	var _meta = {
+			interfaces : [],
+			
+			defaultAggregation : "content",
+			
+			library : "ui5strap",
+
+			properties : { 
+				
+			},
+			
+			aggregations : { 
+				content : {
+					singularName: "content"
+				}
+			}
+
+		};
+	
+	SelectableSupport.meta(_meta);
+	
+	var ListItemBase = ControlBase.extend("ui5strap.ListItemBase", {
+		metadata : _meta
+	}),
+	ListItemBaseProto = ListItemBase.prototype;
+
+	SelectableSupport.proto(ListItemBaseProto);
+	
+	return ListItemBase;
+});
