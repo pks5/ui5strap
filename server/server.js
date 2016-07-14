352503d324714c8cafb535965d9402f1efaaef04
Jan Philipp Knoeller <info@pksoftware.de>
2016-07-14 20:25:44 +0200

    server, demoapp cuscon

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
 var express    =    require('express'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	nodeFs = require('fs');
+
 var app        =    express();
 
-
-app.use('/', express.static(__dirname + '/../www'));
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
 require('./router/main')(app);
 
-var server     =    app.listen(8282,function(){
-    console.log("We have started our server on port 8282");
+nodeFs.readFile(__dirname + "/server.json", 'utf8', function(err, file) {
+	if (err) {
+		console.error("Could not load server.json!", err);
+		return;
+	}
+	
+	var serverConfig = JSON.parse(file);
+	
+	app.use('/', express.static(__dirname + "/" + serverConfig.server.pathToPublic));
+	
+	var server     =    app.listen(serverConfig.server.port,function(){
+	    console.log("Serving '" + serverConfig.server.pathToPublic + "' on port " + serverConfig.server.port);
+	});
 });
