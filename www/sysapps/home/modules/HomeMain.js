1f5c6d65766759478adcfaaeeb6809374c6e6f33
Jan Philipp Knoeller <info@pksoftware.de>
2016-07-28 21:03:57 +0200

    close app

--- www/sysapps/home/modules/HomeMain.js
+++ www/sysapps/home/modules/HomeMain.js
@@ -40,5 +40,71 @@ sap.ui.define(['ui5strap/library', 'ui5strap/Manager'], function(uLib, Manager){
 	  jQuery('body').addClass('ui5strap-with-bar-home');
   };
   
+  /**
+	 * Triggered when the Viewer is resized.
+	 */
+  HomeMainProto.onShow = function(oEvent){
+		
+		var rootControl = this.getApp().getRootControl(),
+			currentPage = rootControl.getTarget("content");
+		
+		if(currentPage && currentPage.getId() === this.getApp().config.createControlId("Home")){
+			currentPage.byId("homeScreen").updateGrid();
+		}
+	};
+  
+  /**
+	 * Triggered when the Viewer is resized.
+	 */
+  HomeMainProto.onResize = function(oEvent){
+		var currentPage = this.getApp().getRootControl().getTarget("content");
+		if(currentPage && currentPage.getId() === this.getApp().config.createControlId("Home")){
+			currentPage.byId("homeScreen").updateGrid();
+		}
+	};
+  
+	/**
+	* Hide, stop and unload an App.
+	*/
+	HomeMainProto.closeApp = function(sappId, callback){
+		var viewer = this.app.getViewer();
+		
+		if(!sappId){
+			throw new Error("Cannot close app: no App ID provided.");
+		}
+
+		var appInstance = viewer.getApp(sappId);
+
+		if(null === appInstance){
+			throw new Error('Cannot close app "' + sappId + '" - app not loaded.');
+		}
+
+		if ( viewer.getApp() === appInstance ) {
+			this.hideApp('transition-zoom2', function hideAppComplete(){
+				viewer.stopApp(sappId);
+				viewer.unloadApp(sappId);
+				callback && callback();
+			});
+		}	
+		else{ 
+			viewer.stopApp(sappId);
+			viewer.unloadApp(sappId);
+
+			callback && callback();
+		}
+	};
+	
+	/**
+	 * Hide an App
+	 */
+	HomeMainProto.hideApp = function(transitionName, callback){
+		var _this = this
+		this.app.getViewer().showApp(this.app.getId(), transitionName, function showAppComplete(appInstance){
+			_this.onResize();
+			callback && callback();
+			//appInstance.hidden();
+		});
+	};
+	
   return HomeMain;
 });
