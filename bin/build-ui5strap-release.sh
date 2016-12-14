5f54ee82375ca266221f4182acc506f64a28be99
pksoftware <info@pksoftware.de>
2016-12-14 21:27:12 +0100

    build

---
+++ bin/build-ui5strap-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo '  __  ___ ____ ______              '
+echo ' / / / (_) __// __/ /________ ____ ' 
+echo '/ /_/ / /__ \_\ \/ __/ __/ _ `/ _ \'
+echo '\____/_/____/___/\__/_/  \_,_/ .__/'
+echo '                            /_/    '                                        
+
+echo
+
+curDir=$(pwd)
+
+echo Building Ui5Strap release in docs ...
+
+cd "$(dirname "$0")"
+
+npm run build-ui5strap-release
+
+cd $curDir
+
+echo 
+
+read -p "Finished. Press any key to continue or wait 10 seconds ..." -t 10
---
+++ build/pks.ui5strap/release/ui5strap-release.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		
+		pathToMinLib : "../../../www/lib/pks/ui5strap_min/",
+		pathToDocsRelease : "../../../../ui5strap-docs/www/lib/ui5strap_min_release/",
+		
+		/*
+		 * Deletes all generated libraries.
+		 */
+		clean : {
+			options : {
+				force : true
+			},
+			
+			cleanDocsRelease : {
+				src : ["<%= pathToDocsRelease %>"]
+			}
+		},
+		
+		/*
+		 * File copy.
+		 */
+		copy : {
+			copyMinLibToDocs: {
+				expand : true,
+				cwd : "<%= pathToMinLib %>",
+				src : ['**'],
+				dest : "<%= pathToDocsRelease %>"
+			}
+		}
+
+	});
+
+	//Load the plugins.
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	
+	/*
+	 * START Define Tasks
+	 */
+	
+	//Clean releases in docs.
+	grunt.registerTask("cleanup", [ "clean:cleanDocsRelease" ]);
+	
+	//Creates a complete release.
+	grunt.registerTask("default", [ "clean:cleanDocsRelease", "copy:copyMinLibToDocs" ]);
+	
+	/*
+	 * END Define Tasks
+	 */
+};
